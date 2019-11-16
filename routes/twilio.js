9d7b39a50903daa110d9e404f34bfaf34310b943
mateo <m.devia@uniandes.edu.co>
2019-11-16 17:42:25 -0500

    ne message

--- routes/twilio.js
+++ routes/twilio.js
@@ -7,7 +7,7 @@ router.post("/newmessage", (req, res) => {
   const newMessage = req.body.Body;
   const usuario = req.body.From;
   let partes = newMessage.split(".");
-  let ids = "\n";
+  let ids = "Sus código:\n";
   for (let i = 0; i < parseInt(partes[1]); i++) {
     ids =
       ids +
